216a0024b1ca6a5b760199411b35ea5a35d62ee1
Yasaman-dt <yasaman.dehghani74@gmail.com>
2026-05-19 17:07:18 -0400

    update

--- plots_tables/results_plots_n_samples_ViT.py
+++ plots_tables/results_plots_n_samples_ViT.py
@@ -335,7 +335,7 @@ for ds in ["cifar10", "cifar100", "tinyimagenet"]:
     ax_aus.yaxis.set_major_formatter(FormatStrFormatter("%.1f"))
     
     ax_last = g_ds.axes[0, 1]
-    ax_last.legend(handles, labels, title="method",
+    ax_last.legend(handles, labels, title="Method",
                    loc="lower right", frameon=True, ncol=1)
     
     plot_with_black_box(g_ds, f"plot_n_sample_ViT_{ds}")
